3b386722071c1910575c6190bc392f24624a1ed3
Ismo Torvinen <ismo.torvinen@gmail.com>
2020-04-12 15:05:55 +0300

    remake of the project - old version did not build correctly - subscribe and callback logic updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default (crude, ids) => (ConnectedComponent) => {
+  return class Connect extends Component {
+    constructor(props) {
+      super(props);
+      this.handleStateChanged = this.handleStateChanged.bind(this);
+      this.state = crude.getState();
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount() {
+      if (!ids) {
+        crude.addStateChangedCallback(this.handleStateChanged);
+      } else {
+        crude.subscribe(ids, this.handleStateChanged);
+      }
+    }
+    componentWillUnmount() {
+      if (!ids) {
+        crude.removeStateChangedCallback(this.handleStateChanged);
+      } else {
+        crude.unsubscribe(ids, this.handleStateChanged);
+      }
+    }
+    handleStateChanged() {
+      this.setState(crude.getState());
+    }
+
+    render() {
+      return <ConnectedComponent {...this.props} state={this.state} />;
+    }
+  };
+};
